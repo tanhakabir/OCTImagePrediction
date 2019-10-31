202585a1d27c330347b03c82c8731c752aca7409
Tanha <tanhakabir.ca@gmail.com>
2019-10-31 03:04:12 -0700

    clean up remove old random walker

--- main.py
+++ main.py
@@ -32,22 +32,6 @@ def produceContrasted2(filename):
 	utils.saveImage(filename + '_output2', processed)
 	return processed
 
-
-def produceRandomWalkerSegmentation(filename, x, y, radius, contrastFn):
-	# Exclude last point because a closed path should not have duplicate points
-	points = utils.circle_points(200, [y, x], radius)[:-1]
-
-	gray = contrastFn(filename)
-
-	image_labels = np.zeros(gray.shape, dtype=np.uint8)
-	indices = draw.circle_perimeter(y, x, 20)#from here
-	image_labels[indices] = 1
-	image_labels[points[:, 1].astype(np.int), points[:, 0].astype(np.int)] = 2
-
-	image_segmented = seg.random_walker(gray, image_labels)
-
-	utils.saveImage(filename + '_rand_walk_' + str(x) + '_' + str(y) + '_' + str(radius), image_segmented)
-
 def produceRandomWalkerSegmentation(filename, x, y, contrastFn):
 	gray = contrastFn(filename)
 
@@ -66,7 +50,4 @@ def produceRandomWalkerSegmentation(filename, x, y, contrastFn):
 
 image = utils.readImageGrayscaled('sample')
 produceRandomWalkerSegmentation('sample', 250, 230, produceContrasted2)
-
-produceRandomWalkerSegmentation('sample4', 240, 200, 250, produceContrasted2)
-produceRandomWalkerSegmentation('sample4', 240, 230, 250, produceContrasted2)
 
