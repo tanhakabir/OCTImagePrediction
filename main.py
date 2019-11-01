b41a57eb9acedc2558c6a7f6fbe562f5012b2270
Tanha <tanhakabir.ca@gmail.com>
2019-10-31 19:58:01 -0700

    refractor basic walker

--- main.py
+++ main.py
@@ -19,34 +19,36 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy import ndimage
 
-def produceRandomWalkerSegmentation(filename, x, y, r):
+RAND_WALK_X = 250
+RAND_WALK_Y = 240
+RAND_WALK_R = 10
+
+def produceRandomWalkerSegmentation(filename):
 	image = utils.readImageGrayscaled(filename)
 	utils.saveImage(filename, 'original_gs', image)
+	og = utils.readImageGrayscaled(filename)
 
 	processed = processing.produceContrast3Tone(image)
 	utils.saveImage(filename, 'contrast_3_tone_processed', processed)
 
 	image_labels = np.zeros(processed.shape, dtype=np.uint8)
-	indices_mid = draw.circle_perimeter(y, x, r)#from here
+	indices_mid = draw.circle_perimeter(RAND_WALK_Y, RAND_WALK_X, RAND_WALK_R) #from here
 	image_labels[indices_mid] = 1
 	image_labels[90,:] = 2
 	image_labels[300,:] = 2
 
 	image_segmented = seg.random_walker(processed, image_labels)
-	utils.saveImage(filename, 'rand_walk_band_' + str(x) + '_' + str(y) + '_' + str(r), image_segmented);
+	utils.saveImage(filename, 'rand_walk_band_' + str(RAND_WALK_X) + '_' + str(RAND_WALK_Y) + '_' + str(RAND_WALK_R), image_segmented);
 
-	print('saved ' + filename + '_rand_walk_band_' + str(x) + '_' + str(y) + '_' + str(r))
+	merged = og + image_segmented
+
+	utils.saveImage(filename, 'merge_rand_walk_band_' + str(RAND_WALK_X) + '_' + str(RAND_WALK_Y) + '_' + str(RAND_WALK_R), merged);
+
+	print('saved ' + filename + '_rand_walk_band_' + str(RAND_WALK_X) + '_' + str(RAND_WALK_Y) + '_' + str(RAND_WALK_R))
 
 
-image = utils.readImageGrayscaled('sample')
-produceRandomWalkerSegmentation('sample', 250, 230, 20)
-
-image = utils.readImageGrayscaled('sample2')
-produceRandomWalkerSegmentation('sample2', 250, 230, 20)
-
-image = utils.readImageGrayscaled('sample3')
-produceRandomWalkerSegmentation('sample3', 250, 230, 20)
-
-image = utils.readImageGrayscaled('sample4')
-produceRandomWalkerSegmentation('sample4', 250, 220, 20)
+produceRandomWalkerSegmentation('sample')
+produceRandomWalkerSegmentation('sample2')
+produceRandomWalkerSegmentation('sample3')
+produceRandomWalkerSegmentation('sample4')
 
